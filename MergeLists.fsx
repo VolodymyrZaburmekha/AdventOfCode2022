b7c037c2661ef20630a6ea8e4db228b99b71c872
Volodymyr Zaburmekha <positive.engine@gmail.com>
2023-06-03 23:15:59 +0300

    refactored loop for leetcode merge function

--- MergeLists.fsx
+++ MergeLists.fsx
@@ -1,11 +1,7 @@
-let merge l1 l2 =
-    let rec loop l1 l2 acc =
-        match l1, l2 with
-        | [], _ -> acc @ l2
-        | _, [] -> acc @ l1
-        | h1 :: t1, h2 :: t2 ->
-            let remainingList1, remainingList2, takenHead = if h1 <= h2 then t1, l2, h1 else l1, t2, h2
-            loop remainingList1 remainingList2 (acc @ [ takenHead ])
-    loop l1 l2 []
+let rec merge l1 l2 =
+    match l1, l2 with
+    | [], _ -> l2
+    | _, [] -> l1
+    | h1 :: t1, h2 :: t2 -> if h1 > h2 then h2 :: (merge l1 t2) else h1 :: (merge l2 t1)
 
 merge [ 1; 2; 3; 4 ] [ -1; 0; 1; 1 ] = [ -1; 0; 1; 1; 1; 2; 3; 4 ]
